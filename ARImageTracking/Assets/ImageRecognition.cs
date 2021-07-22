376b97b82742c5a06a32ba69e36346cab5365077
TomRR <resingtr@gmail.com>
2021-07-22 11:35:49 +0200

    Inital Commit

---
+++ ARImageTracking/Assets/ImageRecognition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }    
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var image in args.added)
+        {
+            Debug.Log(image.name);
+        }
+    }
+}
